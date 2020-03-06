119fc2caa60a94afca20b5c52329348e1b0b3446
BlackMaster <pika4y52@rambler.ru>
2020-03-06 21:07:25 +0700

    Adding new function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
+
+void circle()
+{
+	int Radius, Area, Perimetr;
+	printf("Enter Radius");
+	cin >> ("%d", Radius);
+	Area = 3.14 * Radius * Radius;
+	Perimetr = Radius * 2 * 3.14;
+	printf("perimetr: ");
+	cout << ("%d", Perimetr);
+	printf(" area: ");
+	cout << ("%d", Area);
+}
+
 int main()
 {
 	int n;
